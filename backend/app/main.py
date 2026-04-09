2299a8fa35d027254b3f5ce798fcef840828226f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-04-09 10:35:25 +0530

    first commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,36 +8,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+app = FastAPI()
+
+# ✅ MongoDB Atlas connection
 MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
 db = client["movie_db"]
 collection = db["movies"]
 
-
-
-app = FastAPI()
-
-
-try:
-    client = MongoClient("mongodb://localhost:27017/")
-    client.server_info()
-    print("Mongodb connnected successfully")
-except Exception as e:
-    print("mongodb failed",e)
-
+# ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all (for now)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 class Movie(BaseModel):
     title: str
     rating: int
     image: str | None = None
-
-movies = []
 
 @app.get("/")
 def home():
@@ -47,30 +38,27 @@ def home():
 def add_movies(movie: Movie):
     movie_dict = movie.dict()
     result = collection.insert_one(movie_dict)
-    movie_dict["_id"]= str(result.inserted_id)
+    movie_dict["_id"] = str(result.inserted_id)
     return movie_dict
 
 @app.get("/movies")
 def get_movies():
-    movies= []
-
+    movies = []
     for m in collection.find():
         m["_id"] = str(m["_id"])
         movies.append(m)
     return movies
 
 @app.delete("/movies/{id}")
-def delete_movie(id:str):
-    collection.delete_one({"_id":ObjectId(id)})
+def delete_movie(id: str):
+    collection.delete_one({"_id": ObjectId(id)})
     return {"message": "Deleted"}
 
 @app.put("/movies/{id}")
-def update_movie(id:str, movie: Movie):
+def update_movie(id: str, movie: Movie):
     updated_data = movie.dict()
-
     collection.update_one(
         {"_id": ObjectId(id)},
         {"$set": updated_data}
-        
     )
     return {"message": "Movie updated"}
